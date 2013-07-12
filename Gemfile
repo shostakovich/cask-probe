6678dcaa6533f0d43b4f0770c783244dd89a4ae6
Robert Curth <robert@rocu.de>
2013-07-12 18:15:38 +0200

    Cleanup

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 gem 'castic'
 gem 'parser'
 gem 'virtus'
-gem 'curb'
+
+gem 'rspec'
--- test.rb
+++ test.rb
@@ -1,6 +1,5 @@
 require 'castic'
 require 'virtus'
-require 'curb'
 
 class Cask
   include Virtus
@@ -10,30 +9,49 @@ class Cask
   attribute :sha1, String
 end
 
-casks = Dir["Casks/*.rb"].map do |f|
-  c = Castic.new(f, :Cask)
-  
-  cask = {name: c.name}
-  c.props.each do |prop|
-    attribute = prop[0]
-    value = prop[1] && prop[1][0]
-    cask[attribute] = value
-  end
+class CaskRepository
+  def self.all
+     Dir["Casks/*.rb"].map do |f|
+      c = Castic.new(f, :Cask)
 
-  Cask.new(cask)
+      cask = {name: c.name}
+      c.props.each do |prop|
+        attribute = prop[0]
+        value = prop[1] && prop[1][0]
+        cask[attribute] = value
+      end
+
+      Cask.new(cask)
+    end
+  end
 end
 
-casks.each do |cask|
-  puts "Downloading #{cask.name}"
-  
-  download = Curl::Easy.new(cask.url)
-  download.follow_location = true
-  download.perform
+def sha1(file)
+  `sha1 /tmp/#{cask.name}`
+end
 
-  require 'digest/sha1'
-  if cask.sha1
-    puts "Error" if cask.sha1 != Digest::SHA1.hexdigest(download.body_str)
-  else
-    puts "Success"
+class CaskDownloadChecker
+  attr_reader :cask
+
+  def initialize(cask)
+    @cask = cask
   end
+
+  def check
+    puts "Downloading #{cask.name}"
+
+    download_path =  "/tmp/#{cask.name}"
+    `curl -Ls #{cask.url} > #{download_path}`
+
+    require 'digest/sha1'
+    if cask.sha1
+      false if cask.sha1 != sha1(download_path)
+    else
+      true
+    end
+  end
+end
+
+CaskRepository.all.each do |cask|
+  puts CaskDownloadChecker.new(cask).check
 end
